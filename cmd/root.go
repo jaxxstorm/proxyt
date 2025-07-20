f9d31e429bd9b70a52c5565cad1150dc7ad46c5d
Lee Briggs <lee@leebriggs.co.uk>
2025-07-20 12:01:04 -0700

    prep for first release
    Signed-off-by: Lee Briggs <lee@leebriggs.co.uk>

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"os"
 
+	"github.com/charmbracelet/fang"
+	"github.com/jaxxstorm/vers"
 	"github.com/spf13/cobra"
 )
 
@@ -22,7 +24,7 @@ by proxying requests to the actual Tailscale control plane.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	err := fang.Execute(rootCmd.Context(), rootCmd, fang.WithVersion(GetVersion()))
 	if err != nil {
 		os.Exit(1)
 	}
@@ -30,4 +32,31 @@ func Execute() {
 
 func init() {
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.proxyt.yaml)")
+}
+
+// Version is the version of this tool.
+var Version string
+
+// GetVersion returns the version using the same logic as the version command
+func GetVersion() string {
+	v := Version
+	// If we haven't set a version with linker flags, calculate from git
+	if v == "" {
+		repo, err := vers.OpenRepository(".")
+		if err != nil {
+			return "unknown"
+		}
+
+		opts := vers.Options{
+			Repository: repo,
+			Commitish:  "HEAD",
+		}
+
+		versions, err := vers.Calculate(opts)
+		if err != nil {
+			return "unknown"
+		}
+		v = versions.SemVer
+	}
+	return v
 }
